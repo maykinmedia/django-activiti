71036cbaef53c92e549b706b2a3a59b3ba18cc34
Sergei Maertens <sergei@maykinmedia.nl>
2020-02-24 15:14:13 +0100

    :white_check_mark: Adding tests

---
+++ tests/test_config_model.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+
+import pytest
+import requests_mock
+
+from django_activiti.models import ActivitiConfig
+
+
+@pytest.mark.django_db
+def test_config_model_url_ok():
+    config = ActivitiConfig.get_solo()
+
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://activiti.example.com/activiti-app/api/management/engine",
+            status_code=200,
+        )
+
+        config.clean()
+
+
+@pytest.mark.django_db
+def test_config_model_url_not_ok():
+    config = ActivitiConfig.get_solo()
+
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://activiti.example.com/activiti-app/api/management/engine",
+            status_code=401,
+        )
+
+        with pytest.raises(ValidationError):
+            config.clean()
